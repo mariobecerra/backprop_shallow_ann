babfbdd9c4625b7ecdda8ed9dbd0d1d38afcb4c0
mbc <mbecerracontreras@gmail.com>
2018-01-04 11:41:39 -0600

    Rcpp working (stupid square brackets)

--- compute_gradient.cpp
+++ compute_gradient.cpp
@@ -30,17 +30,17 @@ List compute_gradient(NumericMatrix X,
       sum_beta0 = 0;
       
       for(int k = 0; k < m; k++){
-        pk_minus_yk = p_hat[k] - y[k];
-        dbeta = pk_minus_yk*A[k,l];
-        dbeta0_vec[k] = pk_minus_yk;
-        dbeta_vec[k] = dbeta;
-        dtheta_vec[k] = dbeta*beta[l+1]*(1-A[k,l])*X[k,n];
+        pk_minus_yk = p_hat(k) - y(k);
+        dbeta = pk_minus_yk*A(k,l);
+        dbeta0_vec(k) = pk_minus_yk;
+        dbeta_vec(k) = dbeta;
+        dtheta_vec(k) = dbeta*beta(l+1)*(1-A(k,l))*X(k,n);
       } // end for k to m
       
       // for(int k = 0; k < m; k++){
-      //   pk_minus_yk = p_hat[k] - y[k];
-      //   dbeta = pk_minus_yk*A[k,l];
-      //   sum_theta = sum_theta + dbeta*beta[l+1]*(1-A[k,l])*X[k,n];
+      //   pk_minus_yk = p_hat(k) - y(k);
+      //   dbeta = pk_minus_yk*A(k,l);
+      //   sum_theta = sum_theta + dbeta*beta(l+1)*(1-A(k,l))*X(k,n);
       //   sum_beta = sum_beta + dbeta;
       //   sum_beta0 = sum_beta0 + pk_minus_yk;
       // } // end for k to m
@@ -49,9 +49,9 @@ List compute_gradient(NumericMatrix X,
       // Rcout << "Sum theta: " << sum_theta << std::endl;
       dL_dtheta(n,l) = sum(dtheta_vec)/m;
     } // end for n to nx
-    dL_dbeta[l+1] = sum(dbeta_vec)/m;
+    dL_dbeta(l+1) = sum(dbeta_vec)/m;
   } // end for l to q
-  dL_dbeta[0] = sum(dbeta0_vec)/m;
+  dL_dbeta(0) = sum(dbeta0_vec)/m;
   
   //Rcout << "dtheta: " << dL_dtheta << std::endl;
   
