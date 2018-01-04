44cbed1cee30aa8d452f5a4f69efd958d99ce521
mbc <mbecerracontreras@gmail.com>
2018-01-04 14:05:11 -0600

    Use cppFunction instead of sourceCpp

--- backprop.R
+++ backprop.R
@@ -392,55 +392,52 @@ ann_rcpp <- function(X, y, q = 3, alpha = 0.01, n_iter = 200, init_beta = "rando
 }
 
 
-# 
-# Rcpp::cppFunction(
-# "
-# List compute_gradient(NumericMatrix X, 
-#                       NumericVector p_hat, 
-#                       NumericVector y, 
-#                       NumericVector beta,
-#                       NumericMatrix A) {
-#   int q = beta.size() - 1, nx = X.ncol(), m = X.nrow();
-# 
-#   NumericVector dL_dbeta(q+1);
-#   NumericMatrix dL_dtheta(nx, q);
-# 
-#   for(int i = 0; i < q; i++){
-#     for(int j = 0; j < nx; j++){
-#       dL_dtheta[j,i] = -10.0;
-#     }
-#   }
-# 
-#   double pk_minus_yk;
-#   double sum_beta0;
-#   double sum_theta;
-#   double sum_beta;
-#   for(int l = 0; l < q; l++){
-#     for(int n = 0; n < nx; n++){
-#       sum_theta = -2;
-#       sum_beta = 0;
-#       sum_beta0 = 0;
-#       for(int k = 0; k < m; k++){
-#         pk_minus_yk = p_hat[k] - y[k];
-#         sum_theta = sum_theta + pk_minus_yk*beta[l+1]*A[k,l]*(1-A[k,l])*X[k,n];
-#         sum_beta = sum_beta + pk_minus_yk*A[k,l];
-#         sum_beta0 = sum_beta0 + pk_minus_yk;
-#       } // end for k to m
-#       dL_dtheta[n,l] = sum_theta;
-#     } // end for n to nx
-#     dL_dbeta[l+1] = sum_beta/m;
-#   } // end for l to q
-#   dL_dbeta[0] = sum_beta0/m;
-# 
-#   return List::create(
-#      dL_dbeta,
-#      dL_dtheta
-#   );
-# }
-# "
-# )
 
-Rcpp::sourceCpp("compute_gradient.cpp")
+Rcpp::cppFunction(
+" List compute_gradient(NumericMatrix X, 
+                        NumericVector p_hat, 
+                        NumericVector y, 
+                        NumericVector beta,
+                        NumericMatrix A) {
+    int q = beta.size() - 1, nx = X.ncol(), m = X.nrow();
+    
+    NumericVector dL_dbeta(q+1);
+    NumericMatrix dL_dtheta(nx, q);
+    double dbeta;
+    double pk_minus_yk;
+    double sum_beta0;
+    double sum_theta;
+    double sum_beta;
+
+    for(int l = 0; l < q; l++){
+      for(int n = 0; n < nx; n++){
+
+        sum_theta = 0;
+        sum_beta = 0;
+        sum_beta0 = 0;
+        
+        for(int k = 0; k < m; k++){
+        pk_minus_yk = p_hat(k) - y(k);
+        dbeta = pk_minus_yk*A(k,l);
+        sum_theta = sum_theta + dbeta*beta(l+1)*(1-A(k,l))*X(k,n);
+        sum_beta = sum_beta + dbeta;
+        sum_beta0 = sum_beta0 + pk_minus_yk;
+        } // end for k to m
+        dL_dtheta(n,l) = sum_theta/m;
+        } // end for n to nx
+      dL_dbeta(l+1) = sum_beta/m;
+    } // end for l to q
+    dL_dbeta(0) = sum_beta0/m;
+    
+    return List::create(
+    dL_dbeta,
+    dL_dtheta
+    );
+  }
+"
+)
+
+# Rcpp::sourceCpp("compute_gradient.cpp")
 
 
 ### Example 1
